f24735953b7696de62218211a847243f49a1429b
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-08 16:12:35 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
 
+class Shape{
+    
+  constructor(color){
+    this.color = color;
+  }
+  
+  const drawShape = ()=>{
+    
+  }
+  
+  const calculateArea = ()=>{
+      
+  }
+  
+}
+ export default drawShape;
+ export default calculateArea;
+
 // export class using module.exports
