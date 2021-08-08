af6e343d9205d2b4e4b3e1af4026cf7d7dcf61d6
Amar H. Pujari <47078596+immortal328@users.noreply.github.com>
2021-08-08 16:36:50 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
-
+var shape = require("./shape.js");
 // declare class
+
+class Circle extends shape{
+  constructor(...args){
+    super(...args);
+  }
+  const calculateArea = () =>{
+    console.log("2*Pie*r");
+    return 3;
+  }
+}
 
 
 // export class using module.exports
+module.exports Circle;
